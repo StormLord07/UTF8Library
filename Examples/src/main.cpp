0fde492d82a5f7e8d98a773d3d60cff3aeaca13c
StormLord07 <iljusha1@gmail.com>
2023-09-18 00:35:21 +0300

    changes to library structure

---
+++ Examples/src/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * Created by Ilya 'StormLord07' Repnev
+ * Created on Sun Jul 30 2023
+ */
+
+
+#include <iostream>
+#include "UTF8String.h"
+#include <fstream>
+
+int main() {
+    setlocale(LC_ALL, "");
+    UTF8Lib::UTF8Char ch("✂");
+
+    std::string strg = "✂✂✂";
+    UTF8Lib::UTF8String str(strg);
+
+    std::ofstream outFile("output.txt");
+    if (!outFile) {
+        std::cerr << "Failed to open output file." << std::endl;
+        return 1;
+    }
+
+    std::cout << UTF8Lib::getCharLength("✂"[0]) << std::endl;
+    outFile << '\xEF' << '\xBB' << '\xBF';
+    outFile << ch.toString() << "\n";
+    outFile << str[0].toString() << str[1].toString() << str[2].toString() << "\n";
+    outFile.close();
+
+    return 0;
+}
